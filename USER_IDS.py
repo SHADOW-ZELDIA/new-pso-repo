2ad871aadc88c90b8c52c48c3704c90f08429f1e
No-one-can-find-me <137252829+No-one-can-find-me@users.noreply.github.com>
2023-12-18 21:12:24 +0530

    Delete USER_IDS.py

--- USER_IDS.py
+++
@@ -1,9 +0,0 @@
-from telethon import *
-from telethon.sync import *
-api_id = 1992385
-api_hash = 'a470b85e27ed03b83571c42c499da412'
-with TelegramClient('session', api_id, api_hash) as client:
-    participants = client.get_participants(-1001626684605)
-    user_ids = [participant.id for participant in participants]
-    all_ids = user_ids
-    print(all_ids)
