1d966cc877c825a6ff964caec9a25cae7004a885
No-one-can-find-me <137252829+No-one-can-find-me@users.noreply.github.com>
2024-04-04 18:06:04 +0530

    update

--- byscop.py
+++ byscop.py
@@ -1,5 +1,3 @@
-keyboard=[[InlineKeyboardButton(f"{moves['normal_move']['name']}",callback_data=f'pvpmu_normal_{user_2_id}')],[InlineKeyboardButton(f"{moves['dodge_move']['name']}",callback_data=f'pvpmu_dodge_{user_2_id}'),InlineKeyboardButton(f"SWAP",callback_data=f'pvpmuu_swap_{user_2_id}'),InlineKeyboardButton(f"DRAW",callback_data=f'pvpmuu_draw_{user_1_id}_{user_2_id}')],[InlineKeyboardButton(f"WITHDRAW",callback_data=f'pvpmu_withdraw_{user_1_id}_{user_2_id}')]]
-
 
 async def get_chat_id_from_link(link):
     try:
@@ -13,7 +11,6 @@ async def get_chat_id_from_link(link):
 # Example chat link (replace with your actual link)
 chat_link = 'https://t.me/joinchat/AAAAAEHbEkejzxUjAUCfYg'
 
-chat_id = await get_chat_id_from_link(chat_link)
 if chat_id:
     print(f"Chat ID for the link '{chat_link}' is: {chat_id}")
 else:
